1824d0021c51108d4242ff8e60ef9f88329cdb46
shawky94 <ahmed.shawky5300@gmail.com>
2022-12-23 02:30:44 +0400

    chore: add iam roles to lambda

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,16 +1,26 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, "HealthCheckHandler", {
+    const lambdaFunction = new lambda.Function(this, "HealthCheckHandler", {
       runtime: lambda.Runtime.NODEJS_16_X,
       code: lambda.Code.fromAsset(`lambda/build`),
       handler: "healthCheck.handler",
       functionName: "healthCheck",
     });
+
+    // add roles to lambda
+    lambdaFunction.role?.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess")
+    );
+
+    lambdaFunction.role?.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSESFullAccess")
+    );
   }
 }
