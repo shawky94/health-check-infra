3c9751d83145bbbb092d3a7ca104882cc4b79557
shawky94 <ahmed.shawky5300@gmail.com>
2022-12-23 18:35:17 +0400

    chore: centralize config file

--- bin/infrastructure.ts
+++ bin/infrastructure.ts
@@ -4,7 +4,12 @@ import * as cdk from "aws-cdk-lib";
 import { DbStack } from "../lib/db-stack";
 import { LambdaStack } from "../lib/lambda-stack";
 
+// config
 const regions = ["us-east-1", "us-west-1"];
+const mainRegion = "us-east-1";
+////////
+
+const runInMultipleRegions = regions.length > 1 ? "true" : "false";
 
 const app = new cdk.App();
 // global stack
@@ -12,9 +17,15 @@ new DbStack(app, "DbStack");
 
 // regional stack
 regions.forEach((region) => {
-  new LambdaStack(app, `LambdaStack-${region}`, {
-    env: {
-      region: region,
-    },
-  });
+  new LambdaStack(
+    app,
+    `LambdaStack-${region}`,
+    runInMultipleRegions,
+    mainRegion,
+    {
+      env: {
+        region: region,
+      },
+    }
+  );
 });
--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -4,8 +4,18 @@ import * as iam from "aws-cdk-lib/aws-iam";
 import * as events from "aws-cdk-lib/aws-events";
 import * as targets from "aws-cdk-lib/aws-events-targets";
 
+enum DeploymentType {
+  SINGLE_REGION = "SINGLE_REGION",
+  MULTI_REGION = "MULTI_REGION",
+}
 export class LambdaStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: cdk.App,
+    id: string,
+    runInMultipleRegions: string = "false",
+    mainRegion: string = "us-east-1",
+    props?: cdk.StackProps
+  ) {
     super(scope, id, props);
 
     // defines an AWS Lambda resource
@@ -14,6 +24,10 @@ export class LambdaStack extends cdk.Stack {
       code: lambda.Code.fromAsset(`lambda/build`),
       handler: "healthCheck.handler",
       functionName: "healthCheck",
+      environment: {
+        RUN_IN_MULTI_REGIONS: runInMultipleRegions,
+        MAIN_REGION: mainRegion,
+      },
     });
 
     // add roles to lambda
