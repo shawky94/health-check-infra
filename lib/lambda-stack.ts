98e6c10f7735cd390d6e94a21491e914b1ba35c4
shawky94 <ahmed.shawky5300@gmail.com>
2022-12-23 15:02:38 +0400

    chore: added cron job to lambda

--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -22,5 +24,12 @@ export class LambdaStack extends cdk.Stack {
     lambdaFunction.role?.addManagedPolicy(
       iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSESFullAccess")
     );
+
+    // Run lambda every five minutes
+    const rule = new events.Rule(this, "Rule", {
+      schedule: events.Schedule.expression("rate(5 minutes)"),
+    });
+
+    rule.addTarget(new targets.LambdaFunction(lambdaFunction));
   }
 }
